a8dca33fc8bb12c3759562730c84dc41ec1b3e8b
Peter Murwa <wanjala.pet@gmail.com>
2017-05-03 18:51:03 +0300

    Initial Commit

---
+++ database/migrations/2017_05_03_154255_create_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('user_id', false, true);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropForeign('accounts_user_id_foreign');
+        });
+        Schema::dropIfExists('accounts');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+## Mchanga Demo API
